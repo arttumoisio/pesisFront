5d2c436d67ef0f20ec3f29436992a2c67da2c4be
Arttu Moisio <arttu.moisio@tuni.fi>
2020-03-09 15:19:53 +0200

    build script for both

--- build_script.sh
+++ build_script.sh
@@ -1,6 +1,5 @@
 cd C:\Users\Juulia\arttu\pesis\pesisFront
 ng build --prod --output-path=dist
-docker build .
+docker build . -t front
 heroku container:push web --app young-beyond-12566
 heroku container:release web --app young-beyond-12566
-
