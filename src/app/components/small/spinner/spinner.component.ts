38f0b6922340434a2d07975e045a5e070b462a64
Arttu Moisio <arttu.moisio@tuni.fi>
2020-03-11 21:41:42 +0200

    opensource image for spinner

--- src/app/components/small/spinner/spinner.component.ts
+++ src/app/components/small/spinner/spinner.component.ts
@@ -11,7 +11,8 @@ export class SpinnerComponent implements OnInit {
   loading = true;
 
   // imageUrl = 'https://karhupesis.fi/wp-content/uploads/2018/05/511_harrastuspallo-2.png';
-  imageUrl = 'https://upload.wikimedia.org/wikipedia/fi/thumb/0/07/Superpesis_vanha_logo.svg/320px-Superpesis_vanha_logo.svg.png';
+  // imageUrl = 'https://upload.wikimedia.org/wikipedia/fi/thumb/0/07/Superpesis_vanha_logo.svg/320px-Superpesis_vanha_logo.svg.png';
+  imageUrl = 'https://freesvg.org/img/Vintage-Baseball-Player-Illustration.png';
 
   constructor(private dataService: DataService) {
     dataService.dataLoadingEmitter.subscribe((loading) => {
