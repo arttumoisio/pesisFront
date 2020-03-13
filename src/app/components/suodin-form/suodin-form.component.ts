e1d4c30dd8bff3edcd2a86e3d86f7ed6931ce138
Arttu Moisio <arttu.moisio@tuni.fi>
2020-03-13 04:16:32 +0200

    tekstisuodatin omaan komponenttiin

---
+++ src/app/components/suodin-form/suodin-form.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/dataservice.service';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
+import { FilterService } from 'src/app/services/filter.service';
+
+@Component({
+  selector: 'app-suodin-form',
+  templateUrl: './suodin-form.component.html',
+  styleUrls: ['./suodin-form.component.css']
+})
+export class SuodinFormComponent implements OnInit {
+  otsikot: string[];
+  voiPoistaa: boolean = false;
+
+  filterForm: FormGroup;
+
+  constructor(private dataService: DataService,
+              private formBuilder: FormBuilder,
+              private fs: FilterService) { }
+
+  ngOnInit(): void {
+    this.otsikot = this.dataService.getOtsikot();
+
+    this.filterForm = this.formBuilder.group({
+      string: this.formBuilder.control(''),
+      column: this.formBuilder.control('')
+    });
+    this.dataService.dataChangedEmitter.subscribe(()=>{
+      this.otsikot = this.dataService.getOtsikot();
+    });
+  }
+
+  onSuodata () {
+    console.log('Sent', this.filterForm.value);
+    let string = this.filterForm.value.string;
+    let column = this.filterForm.value.column;
+    this.fs.setStrFilter(
+      {string:string,
+        operator:'',
+        column:column}
+    );
+    this.dataService.dataChangedEmitter.emit();
+  }
+
+  get strings() {
+    return this.filterForm.get('strings') as FormArray;
+  }
+  get columns() {
+    return this.filterForm.get('columns') as FormArray;
+  }
+
+}
--- src/app/components/visualisointi/visualisointi.component.ts
+++ src/app/components/visualisointi/visualisointi.component.ts
@@ -9,6 +9,7 @@ import { DataService } from 'src/app/services/dataservice.service';
 export class VisualisointiComponent implements OnInit {
   errorMessage = '';
   loading = true;
+  piilotaValikko = false;
 
   constructor (private dataService: DataService) {
   }
@@ -19,6 +20,9 @@ export class VisualisointiComponent implements OnInit {
     });
     this.dataService.errorEmitter.subscribe((errMsg: string) => {
       this.errorMessage = errMsg;
+    });
+    this.dataService.piilotusEmitter.subscribe(()=>{
+      this.piilotaValikko = !this.piilotaValikko;
     });
   }
 }
